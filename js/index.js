98b2afec416f567def551039120faa9c723f6a4d
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-20 12:05:26 -0700

    fixed footer, close and search buttons, header, added to modal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-const options = {};
+const options = {
+    startingTop: "50%",
+    endingTop: "20%",
+    opacity: 0.7
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
