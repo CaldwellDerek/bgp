245174ac8f50ca3f8a76ed418ec51faaf95a70d9
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-20 15:29:24 -0700

    collapsible added for similar games

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
-const options = {
+const modalOptions = {
     startingTop: "50%",
     endingTop: "20%",
     opacity: 0.7
-}
+};
+
+const collapsibleOptions = {
+    accordion: false
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+    var instances = M.Modal.init(elems, modalOptions);
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelector('.collapsible');
+    var instances = M.Collapsible.init(elems, collapsibleOptions);
   });
