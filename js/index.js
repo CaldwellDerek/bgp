a9734122c7ccb58ea40d936d4c0425537e510a22
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-20 11:36:01 -0700

    modal added for addition search options

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const options = {};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
