a9ad0a12b0dba71cf1a8c790142e42168a0f98f7
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-30 11:03:36 -0700

    fixed animation with advanced search bar

--- js/index.js
+++ js/index.js
@@ -1,38 +1,37 @@
+// Activates and Closes advanced search options 
 document.querySelector("#advanced-search").addEventListener("click", () => {
   const body = document.querySelector("body");
   const advSearchContainer = document.querySelector(".advanced-search-container");
 
-  if (body.getAttribute("data-height") == "0"){
-    body.setAttribute("data-height", "1");
-    body.classList.add("slidedown");
-    advSearchContainer.classList.add("appear");
-  } else {
-    body.setAttribute("data-height", "0");
-    advSearchContainer.classList.add("disappear");
-    body.classList.add("slideup");
+  function removeSlidedown() {
+    body.classList.remove("slidedown");
+    body.style.gridTemplateRows = "22rem 26rem auto auto 5rem"
+    advSearchContainer.style.opacity = "1";
+    advSearchContainer.style.visibility = "visible";
   }
 
-  body.addEventListener("animationend", () => {
-    if (body.getAttribute("data-height") == "1"){
-      body.classList.remove("slidedown");
-      body.style.gridTemplateRows =  "22rem 26rem auto auto 5rem";
-    } else {
-      body.classList.remove("slideup");
-      body.style.gridTemplateRows =  "22rem 0rem auto auto 5rem";
-    }
-  })
+  function removeSlideup() {
+    body.classList.remove("slideup");
+    body.style.gridTemplateRows = "22rem 0rem auto auto 5rem"
+    removeStyle();
+  }
 
-  advSearchContainer.addEventListener("animationend", () => {
-    if (body.getAttribute("data-height") == "1"){
-      advSearchContainer.classList.remove("appear");
-      advSearchContainer.style.visibility = "visible";
-      advSearchContainer.style.opacity = "1";
-    } else {
-      advSearchContainer.classList.remove("disappear");
-      advSearchContainer.style.visibility = "hidden";
-      advSearchContainer.style.opacity = "0";
-    }
-  })
+  function removeStyle() {
+    advSearchContainer.style.opacity = "0";
+    advSearchContainer.style.visibility = "hidden";
+  }
 
+  if (body.getAttribute("data-trigger") == "down"){
+    advSearchContainer.style.display = "grid";
+    body.classList.add("slidedown");
+    body.addEventListener("animationend", removeSlidedown);
+    body.setAttribute("data-trigger", "up");
+  } else {
+    body.classList.add("slideup");
+    body.addEventListener("animationstart", removeStyle);
+    body.addEventListener("animationend", removeSlideup);
+    body.setAttribute("data-trigger", "down");
+    advSearchContainer.style.display = "none";
+  }
 
 });
