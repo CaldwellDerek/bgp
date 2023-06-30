f566c159bde885588fdad67a5dcffa67b85a60da
Derek Caldwell <djohncaldwell@gmail.com>
2023-06-29 19:52:44 -0700

    added animation for advanced search bar

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
-document.querySelector("#advanced-search").addEventListener("click", (event)=> {
-  event.preventDefault();
-  const advancedSearch = document.querySelector(".advanced-search-div");
+document.querySelector("#advanced-search").addEventListener("click", () => {
+  const body = document.querySelector("body");
+  const advSearchContainer = document.querySelector(".advanced-search-container");
 
-  if (advancedSearch.style.display != "block"){
-    advancedSearch.style.display = "block";
+  if (body.getAttribute("data-height") == "0"){
+    body.setAttribute("data-height", "1");
+    body.classList.add("slidedown");
+    advSearchContainer.classList.add("appear");
   } else {
-    advancedSearch.style.display = "none";
+    body.setAttribute("data-height", "0");
+    advSearchContainer.classList.add("disappear");
+    body.classList.add("slideup");
   }
 
+  body.addEventListener("animationend", () => {
+    if (body.getAttribute("data-height") == "1"){
+      body.classList.remove("slidedown");
+      body.style.gridTemplateRows =  "22rem 26rem auto auto 5rem";
+    } else {
+      body.classList.remove("slideup");
+      body.style.gridTemplateRows =  "22rem 0rem auto auto 5rem";
+    }
+  })
+
+  advSearchContainer.addEventListener("animationend", () => {
+    if (body.getAttribute("data-height") == "1"){
+      advSearchContainer.classList.remove("appear");
+      advSearchContainer.style.visibility = "visible";
+      advSearchContainer.style.opacity = "1";
+    } else {
+      advSearchContainer.classList.remove("disappear");
+      advSearchContainer.style.visibility = "hidden";
+      advSearchContainer.style.opacity = "0";
+    }
+  })
+
+
 });
-
-document.querySelector(".close-search").addEventListener("click", (event) => {
-  event.preventDefault();
-  document.querySelector(".advanced-search-div").style.display = "none";
-
-  
-})
